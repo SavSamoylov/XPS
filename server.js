448bafe19cc240c2e262b402e1d88808b8b9e105
Saveliy <samoylov3@hotmail.com>
2017-08-07 18:13:45 -0400

    Testing functionality and validation.

--- server.js
+++ server.js
@@ -4,10 +4,19 @@ const app = xps.app();
 
 xps.go(app,
   {
-    staticView: "views" ,
-    bodyParse: true,//{json, raw, url, text} - Pass an object to set the exact parse methods you'd want to use.
-    port: 3000
+    staticView: "views", // Also takes an array of strings for multiple view folders.
+    bodyParse: ["json", "raw", "urlencoded", "text"], // TRUE or [ARRAY]  - Pass an object to set the exact parse methods you'd want to use.
+    port: 3000 // Either an INT or STRING
+
     // Can add some other common Middleware.
+    /**
+    -Cookie-Session
+    -Cookie-Parser
+    -Session
+    -Serve-Favicon
+    // Open to further Development
+
+    **/
   }
 )
 
--- xps.js
+++ xps.js
@@ -7,44 +7,128 @@ let XPS = function(){
 
   this.go = function(app, obj){
 
+    //==================================================================================================
+    // Set the Static file routes.
+    //==================================================================================================
 
-    if(obj.staticView){
-      let directory = obj.staticView;
-      console.log(directory)
-      app.use(express.static(directory));
+    if(obj.staticView && obj.staticView !== ""){
+      let xps_Dir = obj.staticView;
+      let xps_DirType = typeof xps_Dir;
+
+      if (xps_DirType === "string"){
+
+        f_xpsStatic(xps_Dir);
+
+      } else if (xps_DirType === "object"){
+
+        for (var i = 0; i < xps_Dir.length; i++) {
+
+          f_xpsStatic(xps_Dir[i].toString())
+        }
+
+      } else {
+        console.log("ERROR: The staticView property only takes  a string or an array of strings value.");
+        return
+      }
+
+
     }
 
+    //==================================================================================================
+    // Set Body Parser Options
+    //==================================================================================================
 
     if(obj.bodyParse){
 
-        let bp = obj.bodyParse;
-        console.log(bp)
+      let xps_BP = obj.bodyParse;
+      let xps_BPType = typeof xps_BP;
+      console.log(xps_BP)
 
-        if(bp === true){
-          // parse various different custom JSON types as JSON
-          app.use(bodyParser.json({ type: 'application/*+json' }))
+      if(xps_BP === true){
+        // parse various different custom JSON types as JSON
+        app.use(bodyParser.json({ type: 'application/*+json' }))
 
-          // parse application/x-www-form-urlencoded
-          app.use(bodyParser.urlencoded({ extended: false }))
+        // parse application/x-www-form-urlencoded
+        app.use(bodyParser.urlencoded({ extended: false }))
 
-          // parse some custom thing into a Buffer
-          app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+        // parse some custom thing into a Buffer
+        app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 
-          // parse an HTML body into a string
-          app.use(bodyParser.text({ type: 'text/html' }))
+        // parse an HTML body into a string
+        app.use(bodyParser.text({ type: 'text/html' }))
+
+      } else if (xps_BPType === "object"){
+
+        for (var i = 0; i < xps_BP.length; i++) {
+
+          if (xps_BP[i].toLowerCase().trim() === "json"){
+            app.use(bodyParser.json({ type: 'application/*+json' }))
+            console.log("JSON")
+          }
+          if (xps_BP[i].toLowerCase().trim() === "urlencoded"){
+            app.use(bodyParser.urlencoded({ extended: false }))
+            console.log("URL")
+          }
+          if (xps_BP[i].toLowerCase().trim() === "raw"){
+            app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+            console.log("RAW")
+          }
+          if (xps_BP[i].toLowerCase().trim() === "text"){
+            app.use(bodyParser.text({ type: 'text/html' }))
+            console.log("TEXT")
+          }
+
         }
 
+      }
+
     }
+
+    //==================================================================================================
+    // Set PORT
+    //==================================================================================================
 
     if(obj.port) {
 
-        let PORT = process.env.PORT || obj.port;
-        console.log(PORT);
-        app.listen(PORT, ()=>{
-          console.log("Server started on PORT: %s", PORT)
-        })
+      let PORT = process.env.PORT || obj.port;
+      let PORT_Type = typeof PORT;
+      console.log(PORT);
+      console.log(PORT_Type);
+
+
+      if (PORT_Type === "string" || PORT_Type === "number"){
+        if (isNaN(PORT)) {
+          PORT = parseInt(PORT)
+          app.listen(PORT, ()=>{
+            console.log("Server started on PORT: %s", PORT)
+          })
+        } else {
+          app.listen(PORT, ()=>{
+            console.log("String Server started on PORT: %s", PORT)
+          })
+        }
+      } else {
+        console.log("ERROR: The port property only takes a string or an integer value. Example ('3000' or 3000)")
+        return;
       }
+
     }
+
+
+    //==================================================================================================
+    // XPS Functions
+    //==================================================================================================
+
+    
+    // -------- SET STATIC DIRECTORIES
+
+    function f_xpsStatic(directoryName){
+      app.use(express.static(directoryName.trim().replace(/\s/g,'')));
+    }
+
+
+
+  } // -------- End of xps.go
 
 }
 
