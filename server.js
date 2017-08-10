3842b99b1f2dcac289a64ff992ffbf5ce9791513
Saveliy <samoylov3@hotmail.com>
2017-08-10 02:51:27 -0400

    none

--- server.js
+++ server.js
@@ -8,22 +8,20 @@ xps.go(app,
     viewEngine: "express-handlebars", // So far just, express-handlebars ... but can be extended to include support for Pug (f.k.a Jade) or other Handlebar packages. Takes String.
     bodyParse: ["json", "raw", "urlencoded", "text"], // TRUE or [ARRAY]  - Pass an object to set the exact parse methods you'd want to use.
     port: 3000, // Either an INT or STRING
-    methodOverride: true, // app.use(methodOverride('_method')) ... Doesn't override HTTP Headers... because I don't know what that is.
+    methodOverride: true, //Doesn't override HTTP Headers... because I don't know what that is.
     httpLogger: "morgan",
     favicon: true, // Will look inside the
-
-    // Can add some other common Middleware.
-    // -------------------------------------
-    // -Cookie-Session
-    // -Cookie-Parser
-    // -Session
-    // - etc
-    // -------------------------------------
-    // Open to further Development
-
-
   }
 )
 
 let routes = require(path.join(__dirname, "controller/routes.js"))
 app.use(routes)
+
+// Can add some other common Middleware.
+// -------------------------------------
+// -Cookie-Session
+// -Cookie-Parser
+// -Session
+// - etc
+// -------------------------------------
+// Open to further Development
